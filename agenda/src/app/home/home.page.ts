e1b221f919424f8efa6db3fd54b524ae202ec541
jmf-uniandes <josefc79@uniandes.edu.ec>
2025-01-21 18:54:22 -0500

    implement logic servicio 1.0

--- agenda/src/app/home/home.page.ts
+++ agenda/src/app/home/home.page.ts
@@ -13,10 +13,11 @@ import {
   IonCardContent,
   IonButton,
   LoadingController,
-  IonLoading 
+  IonLoading,
 } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
-
+import { AccesoService } from '../servicios/acceso.service';
+AccesoService;
 
 @Component({
   selector: 'app-home',
@@ -36,25 +37,46 @@ import { FormsModule } from '@angular/forms';
     IonCardContent,
     IonButton,
     IonLoading,
-    FormsModule 
+    FormsModule,
   ],
 })
 export class HomePage {
-  txt_usuario: string = "";
-  txt_clave: string = "";
+  txt_usuario: string = '';
+  txt_clave: string = '';
   constructor(
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private servicio: AccesoService
   ) {}
-  
+
   login() {
-   
+    let datos = {
+      accion: 'login',
+      usuario: this.txt_usuario,
+      clave: this.txt_clave,
+    };
+    this.servicio.postData(datos).subscribe(
+      (res: any) => {
+        //   if(response['success']==true){
+        //     this.servicio.showToast(response['msg'],2000);
+        //     this.servicio.createSession('usuario',response['usuario']);
+        //     this.servicio.createSession('nombre',response['nombre']);
+        //     this.servicio.createSession('correo',response['correo']);
+        //   }else{
+        //     this.servicio.showToast(response['msg'],2000);
+        //   }
+        if (res.estado) {
+          this.servicio.showToast('Encontro persona', 3000);
+        } else {
+          this.servicio.showToast('No existe persona', 3000);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  createUser(){
-
-  }
-  recoverPassword(){
-
-  }
+  createUser() {}
+  recoverPassword() {}
   async showLoading() {
     const loading = await this.loadingCtrl.create({
       message: 'Dismissing after 3 seconds...',
