a2e302e54f1f20ce02325d2c640482bb324117a4
jmf-uniandes <josefc79@uniandes.edu.ec>
2025-01-23 21:26:16 -0500

    class jan 23, 2025

--- agenda/src/app/account/account.page.ts
+++ agenda/src/app/account/account.page.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonItem,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonItem,
+  ],
 })
 export class AccountPage implements OnInit {
+  txt_cedula: string = '';
+  txt_name: string = '';
+  txt_surename: string = '';
+  txt_correo: string = '';
+  txt_clave: string = '';
+  txt_cclave: string = '';
+  mensaje: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  vclave() {}
+  registrar() {}
+  cancelar() {}
 }
